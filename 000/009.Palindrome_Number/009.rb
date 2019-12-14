21c85def20f1fc21df7bc217cfa7bab9f4ce96f9
castor4bit <castor.4bit@gmail.com>
2019-12-14 12:35:56 +0900

    without converting the integer to a string

--- 000/009.Palindrome_Number/009.rb
+++ 000/009.Palindrome_Number/009.rb
@@ -1,5 +1,20 @@
 # @param {Integer} x
 # @return {Boolean}
 def is_palindrome(x)
-  x == x.to_s.reverse.to_i
+  return false if x < 0
+
+  a = x
+  b = 0
+  i = 1
+
+  while a >= b do
+    b = b * 10 + (a % 10)
+    a = (a >= 10 ** i) ? a.div(10) : a
+
+    return true if a == b
+    return false if a < 10 ** i
+    i += 1
+  end
+
+  false
 end
