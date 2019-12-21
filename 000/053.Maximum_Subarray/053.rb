1d96bf22cda79bdf0c1ace3c6fbf5c442a227402
castor4bit <castor.4bit@gmail.com>
2019-12-22 08:08:11 +0900

    slightly faster but still slow...

--- 000/053.Maximum_Subarray/053.rb
+++ 000/053.Maximum_Subarray/053.rb
@@ -1,9 +1,22 @@
 # @param {Integer[]} nums
 # @return {Integer}
 def max_sub_array(nums)
+  idx = 0
+  n = 0
+  while !nums[idx].nil? do
+    m = nums[idx]
+    if idx > 0 and n * m >= 0 then
+      nums[idx] += n
+      nums.delete_at(idx - 1)
+    else
+      idx += 1
+    end
+
+    n = m
+  end
+
   len = 1
   max = nil
-
   while len <= nums.size do
     0.step(nums.size - len) do |n|
       sum = nums.slice(n, len).sum
@@ -15,4 +28,3 @@ def max_sub_array(nums)
 
   max
 end
-
