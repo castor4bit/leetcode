d93f0b2ccc96d464759db63d098a92da6166464a
castor4bit <castor.4bit@gmail.com>
2019-12-26 17:43:49 +0900

    add TreeNodeUtil

---
+++ structures/test_treenode.rb
@@ -0,0 +1,28 @@
+require 'test/unit'
+require './treenode'
+
+class TreeNodeTest < Test::Unit::TestCase
+  def test_treenode
+    node = TreeNode.new(1)
+    assert_equal 1, node.val
+    assert_equal nil, node.left
+    assert_equal nil, node.right
+  end
+end
+
+class TreeNodeUtilTest < Test::Unit::TestCase
+  def test_array_to_treenode
+    head = TreeNodeUtil.array_to_treenode([1, 2, 3])
+    assert_equal 1, head.val
+    assert_equal 2, head.left.val
+    assert_equal 3, head.right.val
+
+    head = TreeNodeUtil.array_to_treenode([3, 9, 20, nil, nil, 15, 7])
+    assert_equal 3, head.val
+    assert_equal 9, head.left.val
+    assert_equal 20, head.right.val
+    assert_equal 15, head.right.left.val
+    assert_equal 7, head.right.right.val
+  end
+end
+
--- structures/treenode.rb
+++ structures/treenode.rb
@@ -5,3 +5,16 @@ class TreeNode
         @left, @right = nil, nil
     end
 end
+
+class TreeNodeUtil
+  def self.array_to_treenode(a, pos = 1)
+    val = a[pos - 1]
+    return nil if val.nil?
+
+    node = TreeNode.new(val)
+    node.left = array_to_treenode(a, pos * 2)
+    node.right = array_to_treenode(a, pos * 2 + 1)
+
+    node
+  end
+end
