4cf99076dfed8d4966fa893c7215b7b96721b1e3
castor4bit <castor.4bit@gmail.com>
2019-12-27 22:58:32 +0900

    amazing.......

--- 100/136.Single_Number/136.rb
+++ 100/136.Single_Number/136.rb
@@ -1,12 +1,5 @@
 # @param {Integer[]} nums
 # @return {Integer}
 def single_number(nums)
-  p = nil
-  c = 0
-  nums.sort.each do |n|
-    return p if !p.nil? and p != n and c == 0
-    c = p == n ? (c + 1) : 0
-    p = n
-  end
-  p
+  2 * nums.uniq.sum - nums.sum
 end
