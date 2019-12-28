e3353017e2c349324f013e00392d825a62a7925f
castor4bit <castor.4bit@gmail.com>
2019-12-28 12:21:59 +0900

    a little improvement..

--- 100/198.House_Robber/198.rb
+++ 100/198.House_Robber/198.rb
@@ -3,15 +3,16 @@
 def rob(nums)
   max = 0
 
-  for i in 1..(2 ** nums.size - 1)
-    next if i.to_s(2).include?("11")
-
-    sum = 0
-    for j in 0..(nums.size - 1)
-      sum += nums[j] if (i >> j) & 1 == 1
+  i = 0
+  while i < (2 ** nums.size - 1)
+    s = i.to_s(2)
+    if s.include?("11") then
+      pos = s.index("11")
+      i += s[pos..-1].gsub(/./, '0').sub(/^../, "1").to_i(2)
+      next
     end
 
-    max = [max, sum].max
+    i += 1
   end
 
   max
