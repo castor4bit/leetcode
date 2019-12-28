842d7ba855db422b9df50d169f141f15b40eb491
castor4bit <castor.4bit@gmail.com>
2019-12-29 01:09:52 +0900

    197. Rising Temperature (MySQL)

---
+++ 100/197.Rising_Temperature/197.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+SELECT w1.Id
+FROM Weather w1
+JOIN Weather w2 ON w2.RecordDate = DATE_SUB(w1.RecordDate, INTERVAL 1 DAY)
+WHERE w1.Temperature > w2.Temperature
