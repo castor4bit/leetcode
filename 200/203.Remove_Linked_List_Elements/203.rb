2aedf176391f563ffefca7b44295b9675727996b
castor4bit <castor.4bit@gmail.com>
2019-12-28 14:25:41 +0900

    203. Remove Linked List Elements (Ruby)

---
+++ 200/203.Remove_Linked_List_Elements/203.rb
@@ -0,0 +1,27 @@
+# Definition for singly-linked list.
+# class ListNode
+#     attr_accessor :val, :next
+#     def initialize(val)
+#         @val = val
+#         @next = nil
+#     end
+# end
+
+# @param {ListNode} head
+# @param {Integer} val
+# @return {ListNode}
+def remove_elements(head, val)
+  node = head
+  while !node.nil? do
+    break if node.next.nil?
+
+    if node.next.val == val then
+      node.next = node.next.next
+    else
+      node = node.next
+    end
+  end
+
+  head = nil if !head.nil? and head.val == val
+  head
+end
---
+++ 200/203.Remove_Linked_List_Elements/test_203.rb
@@ -0,0 +1,19 @@
+require 'test/unit'
+require './203'
+require '../../structures/listnode'
+
+class LeetCodeSolutionTest < Test::Unit::TestCase
+  def test_203
+    head = ListNodeUtil.array_to_listnode([1, 2, 6, 3, 4, 5, 6])
+    assert_equal [1, 2, 3, 4, 5], ListNodeUtil.listnode_to_array(remove_elements(head, 6))
+
+    head = ListNodeUtil.array_to_listnode([])
+    assert_equal [], ListNodeUtil.listnode_to_array(remove_elements(head, 6))
+
+    head = ListNodeUtil.array_to_listnode([1, 2, 3])
+    assert_equal [1, 2, 3], ListNodeUtil.listnode_to_array(remove_elements(head, 6))
+
+    head = ListNodeUtil.array_to_listnode([6, 6])
+    assert_equal [], ListNodeUtil.listnode_to_array(remove_elements(head, 6))
+  end
+end
