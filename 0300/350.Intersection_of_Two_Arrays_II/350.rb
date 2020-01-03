42ad42816a2c06f6fcba5c36acb6ed2d64a8a741
castor4bit <castor.4bit@gmail.com>
2020-01-03 19:27:37 +0900

    improvement

--- 0300/350.Intersection_of_Two_Arrays_II/350.rb
+++ 0300/350.Intersection_of_Two_Arrays_II/350.rb
@@ -2,24 +2,17 @@
 # @param {Integer[]} nums2
 # @return {Integer[]}
 def intersect(nums1, nums2)
-  def count_nums(nums)
-    counts = Hash.new(0)
-    nums.each do |n|
-      counts[n] += 1
-    end
-    counts
-  end
-
-  c1 = count_nums(nums1)
-  c2 = count_nums(nums2)
-
-  (c1.keys + c2.keys).uniq.each do |c|
-    c1[c] = [c1[c], c2[c]].min
-  end
-
   result = []
-  c1.each do |k, v|
-    result += Array.new(v, k)
+  counts = Hash.new(0)
+
+  nums1.each do |n|
+    counts[n] += 1
+  end
+  nums2.each do |n|
+    if counts[n] > 0
+      result.push(n)
+      counts[n] -= 1
+    end
   end
 
   result
