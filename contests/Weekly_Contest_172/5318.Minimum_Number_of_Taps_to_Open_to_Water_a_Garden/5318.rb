ce85c3bf07a449d1a28bf5190e444c8ed6f97da5
castor4bit <castor.4bit@gmail.com>
2020-01-19 14:00:11 +0900

    fixed

--- contests/Weekly_Contest_172/5318.Minimum_Number_of_Taps_to_Open_to_Water_a_Garden/5318.rb
+++ contests/Weekly_Contest_172/5318.Minimum_Number_of_Taps_to_Open_to_Water_a_Garden/5318.rb
@@ -2,36 +2,24 @@
 # @param {Integer[]} ranges
 # @return {Integer}
 def min_taps(n, ranges)
-  def find_longest(target, ranges)
-    max = 0
-    maxidx = nil
-    ranges.each_with_index do |range, x|
-      l = x - range
-      r = x + range
-      if l <= target && target <= r && max < r - target
-        max = r - target
-        maxidx = x
-      end
-    end
+  t = Array.new(n + 1, 0)
+  ranges.each_with_index do |v, i|
+    l = [0, i - v].max
+    r = [i + v, n].min
 
-    maxidx
+    (l..r).each do |j|
+      t[j] = [t[j], r].max
+    end
   end
 
   count = 0
-  watered = Array.new(n + 1, false)
-  watered.each_with_index do |w, target|
-    next if w
-
-    maxidx = find_longest(target, ranges)
-    break if maxidx.nil?
-
-    (target..(maxidx + ranges[maxidx])).each do |i|
-      watered[i] = true
-    end
+  idx = 0
+  while idx < n
+    break if t[idx] == 0
 
     count += 1
-    ranges[maxidx] = 0
+    idx = t[idx]
   end
 
-  watered.all? ? count : -1
+  idx < n ? -1 : count
 end
