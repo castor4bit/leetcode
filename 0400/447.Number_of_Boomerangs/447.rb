423bb6dd8d23baf7b37d108099ad443cebefc306
castor4bit <castor.4bit@gmail.com>
2020-01-22 11:12:41 +0900

    improve performance

--- 0400/447.Number_of_Boomerangs/447.rb
+++ 0400/447.Number_of_Boomerangs/447.rb
@@ -1,25 +1,21 @@
 # @param {Integer[][]} points
 # @return {Integer}
 def number_of_boomerangs(points)
-  return points.size if points.size <= 2
-
-  dd = Array.new(points.size, [])
-  dd.map {|d| d = Array.new(points.size) }
+  #return points.size if points.size <= 2
 
   count = 0
   len = points.size - 1
   (0..len).each do |i|
+    dd = Hash.new(0)
     (0..len).each do |j|
       next if i == j
 
-      (0..len).each do |k|
-        next if i == k || j == k
+      d = (points[j][0] - points[i][0]) ** 2 + (points[j][1] - points[i][1]) ** 2
+      dd[d] += 1
+    end
 
-        dd[i][j] = dd[j][i] = Math.sqrt((points[j][0] - points[i][0]) ** 2 + (points[j][1] - points[i][1]) ** 2) if dd[i][j].nil?
-        dd[i][k] = dd[k][i] = Math.sqrt((points[k][0] - points[i][0]) ** 2 + (points[k][1] - points[i][1]) ** 2) if dd[i][k].nil?
-
-        count += 1 if dd[i][j] == dd[i][k]
-      end
+    dd.values.select {|v| v >= 2}.each do |i|
+      count += (i - 1) * i
     end
   end
 
--- 0400/447.Number_of_Boomerangs/test_447.rb
+++ 0400/447.Number_of_Boomerangs/test_447.rb
@@ -5,5 +5,6 @@ class LeetCodeSolutionTest < Test::Unit::TestCase
   def test447
     assert_equal 2, number_of_boomerangs([[0,0],[1,0],[2,0]])
     assert_equal 8, number_of_boomerangs([[0,0],[1,0],[1,0],[2,0]])
+    assert_equal 0, number_of_boomerangs([[1,1]])
   end
 end
