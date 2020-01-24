56e37f68e64ff8717fb9d568bd4a0ea145d90657
castor4bit <castor.4bit@gmail.com>
2020-01-24 23:47:56 +0900

    improvement performance, but still TLE...

--- 0400/453.Minimum_Moves_to_Equal_Array_Elements/453.rb
+++ 0400/453.Minimum_Moves_to_Equal_Array_Elements/453.rb
@@ -2,14 +2,28 @@
 # @return {Integer}
 def min_moves(nums)
   count = 0
+  mc = 1
   while nums.uniq.size > 1
     nums.sort!
 
-    (0..nums.size - 2).each do |i|
-      nums[i] += 1
+    max = nums[-1]
+    idx = -2
+    while nums[idx] == max
+      mc += 1
+      idx -= 1
     end
+    nums = nums.slice(0..(idx + 1))
 
-    count += 1
+    max2 = nums[-2]
+    d1 = (max - max2).lcm(mc)
+    d2 = d1 * (mc - 1) / mc
+
+    (0..(nums.size - 2)).each do |i|
+      nums[i] += d1
+    end
+    nums[-1] += d2
+
+    count += d1
   end
 
   count
--- 0400/453.Minimum_Moves_to_Equal_Array_Elements/test_453.rb
+++ 0400/453.Minimum_Moves_to_Equal_Array_Elements/test_453.rb
@@ -7,5 +7,15 @@ class LeetCodeSolutionTest < Test::Unit::TestCase
     assert_equal 10, min_moves([1, 2, 10])
     assert_equal 8, min_moves([-5, -4, -3, 0])
     assert_equal 15343, min_moves([1, 2, 4, 50, 43, 546, 6437, -324, -435, 4234, 0])
+    assert_equal 2147483646, min_moves([1, 2147483647])
+
+    nums = eval(File.read('./data1.txt'))
+    assert_equal 101895, min_moves(nums)
+
+    nums = eval(File.read('./data2.txt'))
+    assert_equal 5009418, min_moves(nums)
+
+    nums = eval(File.read('./data3.txt'))
+    assert_equal 5009418, min_moves(nums)
   end
 end
